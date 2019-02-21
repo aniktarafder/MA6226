a36985c28a5036ee7d359d605fc1ca9a7358dff3
aniktarafder <aniktarafder@local>
2019-02-21 19:49:08 +0000

    class6

--- vortex.py
+++ vortex.py
@@ -1,14 +1,6 @@
 import math
 import numpy
 
-width = 10
-height = (y_end - y_start) / (x_end - x_start) * width
-pyplot.figure(figsize=(width, height))
-pyplot.xlabel('x', fontsize=16)
-pyplot.ylabel('y', fontsize=16)
-pyplot.xlim(x_start, x_end)
-pyplot.ylim(y_start, y_end)
-
 def vor(strength, xv, yv, X, Y):
     u = +strength / (2 * math.pi) * (Y - yv) / ((X - xv)**2 + (Y - yv)**2)
     v = -strength / (2 * math.pi) * (X - xv) / ((X - xv)**2 + (Y - yv)**2)
@@ -20,9 +12,3 @@ def vel(kappa, xd, yd, X, Y):
     u = - kappa / (2 * math.pi) * ((X - xd)**2 - (Y - yd)**2) / ((X - xd)**2 + (Y - yd)**2)**2
     v = - kappa / (2 * math.pi) * 2 * (X - xd) * (Y - yd) / ((X - xd)**2 + (Y - yd)**2)**2
     return u, v
-
-def set_fig_style():
-    fig, ax = pyplot.subplot()
-    ax.set_xlabel('x')
-    ax.set_ylable('y')
-    return fig, ax
